9956664718853f4d57b30d51135f7a2eb0829dff
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2018-04-09 20:59:33 -0700

    worked on vars and alerts

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var myHistory = "I am a graphic designer who's interested in UX design and interested in coding. I'd llike to explore a career in Frontend Development. I'm a ninja!";
+
+var myValue = confirm(myHistory);
+
+console.log(myHistory);
+
+console.log(myValue);
