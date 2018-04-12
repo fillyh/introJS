28511c098272540f683940baa0fdbdae47409896
felipe hernandez <Felipe_Sue@felipesMini1764.hsd1.ca.comcast.net>
2018-04-11 22:53:15 -0700

    I think I got it

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,11 @@
-// console.log(myName); /* undefined*/
-// const myName ="felipe";
-// console.log (myName, 1);
-// {
-// const myName = "Christa";
-// console.log(myName, 2);
-// }
-// console.log(myName, 3);
+firstName = prompt("Hey! Didn't I see you at BitWise? What was your first name again?");
+lastName = prompt("Oh! Hi " + firstName + "! So, like, what's your last name?");
 
+console.log(firstName);
 
-// // var age = 32;
-// //
-// // age = new Number (age);
-//
-// console.log(myName.toUpperCase())
-//
-// // myName = new String (myName);
-//
-// // console.log(typeof (myName) );
-//
-// // console.log(myName);
-//
-// // console.log(age);
-//
-// // console.log(age);
-//
-// var test;
-//
-// console.log(test);
+alert(lastName + "!, I knew it!");
 
-// let birthday = "5/21/86";
-// let rightNow = new Date (birthday);
-//
-// alert(rightNow);
+usersBirthday = prompt("I'm not weird or anything, but what's your birthday" + " (Please enter it this way: MM/DD/YYYY ... I'm not that smart)");
+rightNow = new Date (usersBirthday);
 
-// let myString = "Hellow World";
-// alert(myString.length);
-
-// let myObject ={
-//   studentCount: 0,
-//   teacherRating: 1000;
-// }
-
-// let firstName;
-// let lastName;
-//
-// firstName = prompt("Enter First Name");
-// lastName = prompt("Enter Last Name");
-//
-// console.log(lastName);
-//
-// alert("If you were a Knight, your name would be Sir " + lastName);
-
-
-// let yourBirthday = confirm("" prompt("Enter Your Birthday"));
-//
-// console.log(yourBirthday);
-
-const userStory = prompt("");
-console.log(userStory);
+alert(rightNow);
