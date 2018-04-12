2797052622d12b60b53c1f5d3b854dc2442c0d05
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2018-04-11 20:59:03 -0700

    day two notes

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+// console.log(myName); /* undefined*/
+// const myName ="felipe";
+// console.log (myName, 1);
+// {
+// const myName = "Christa";
+// console.log(myName, 2);
+// }
+// console.log(myName, 3);
+
+
+// // var age = 32;
+// //
+// // age = new Number (age);
+//
+// console.log(myName.toUpperCase())
+//
+// // myName = new String (myName);
+//
+// // console.log(typeof (myName) );
+//
+// // console.log(myName);
+//
+// // console.log(age);
+//
+// // console.log(age);
+//
+// var test;
+//
+// console.log(test);
+
+// let birthday = "5/21/86";
+// let rightNow = new Date (birthday);
+//
+// alert(rightNow);
+
+// let myString = "Hellow World";
+// alert(myString.length);
+
+// let myObject ={
+//   studentCount: 0,
+//   teacherRating: 1000;
+// }
+
+// let firstName;
+// let lastName;
+//
+// firstName = prompt("Enter First Name");
+// lastName = prompt("Enter Last Name");
+//
+// console.log(lastName);
+//
+// alert("If you were a Knight, your name would be Sir " + lastName);
+
+
+// let yourBirthday = confirm("" prompt("Enter Your Birthday"));
+//
+// console.log(yourBirthday);
