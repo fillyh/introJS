d9700146093f321207da21a0319348410827ea75
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2018-04-16 20:59:05 -0700

    day three

---
+++ js/scripts.js
@@ -0,0 +1,90 @@
+// let myName = "blah";
+//
+// alert('hello ' + myName + " yay!");
+//
+// /*Backtick Way*/
+//
+// alert(`Hello ${myName}. You are great!`);
+
+
+/*For loops*/
+
+// for (let i = 0; i <10; i++) {
+//   console.log(i);
+// }
+
+// for (let i =0; i<10 || i<15; i++) {
+//   if (!(i%2===0))
+//   console.log(i);
+// }
+
+// // Declare a var and set it equal to any string.
+// var favoriteBook = "Catcher in the Rye";
+// // Declare a second var and call new String() on the same string (this will create an instance of a JS string object.)
+// var aBook = new String( favoriteBook);
+// // Use the logical operators and a third var to determine if they are strictly equal.
+// var isTrue = (favoriteBook==aBook);
+// console.log(isTrue);
+// // Now use the logical operators and a fourth var to determine if they are loosely equal.
+// var isNotTrue = (favoriteBook===aBook);
+// console.log(isNotTrue);
+// // Why did you get the result you got?
+
+// var yourName = prompt('wahts your name?', 'Carlos');
+// console.log(yourName.length);
+// // console.log(!yourName);
+//
+// console.log("1"=1);  /*Reference error*/
+
+// let obj1 ={
+//   name: "Ryeker"
+// };
+//
+// let obj2 = {
+//   name: "Ryeker"
+// };
+//
+// console.log(obj1 === obj2);
+
+// console.log("three" * 15); /*NaN*/
+// console.log(typeof 15);
+// console.log(typeof "three");
+// console.log(typeof("three" * 15)); /*returns "number"*/
+// console.log(isNaN("three" * 15)); /*returns true*/
+
+// let myNumber = prompt("Give me a Number!");
+//
+// alert(
+//   isNaN((parseInt(myNumber) / 2))
+// );
+//
+// let string = 44;
+//
+// string.toString();
+//
+// alert(`your favorite number is ${string}`);
+
+
+
+
+// let yourNumb = prompt("gimme a numba!" , 10);
+//
+// let newNumb = parseInt(yourNumb);
+//
+// console.log('a: ' + (newNumb +42) ); /* could also use += */
+// console.log('b: ' + (newNumb -42) );
+// console.log('c: ' + (newNumb *42) );
+// console.log('d: ' + (newNumb/42) );
+
+
+const best =42;
+const cheese = "delicious";
+
+if(best === "42"){
+  console.log('hooray!!');
+} else if(cheese =="delicious"){
+  console.log("hell yea!");
+}
+else{
+  console.log('boooo');
+}
