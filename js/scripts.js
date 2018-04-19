310fc72a837decf5a2a0d6ac57a600905bddc04f
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2018-04-18 18:54:31 -0700

    day 4 notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,19 @@
-alert("Welcome to the Geekwise Jedi Academy!")
+// feilds
+let emailArr;
+var yourEmail;
 
-let birthday = new Date (prompt("Before we begin, please enter you birthday" , "Ex. 11/12/1985"));
-usersAge = birthday.getFullYear();
-console.log(usersAge);
+while (true) {
+  //user input
+  var yourEmail = prompt('What is your email?');
+  emailArr = yourEmail.split("");
 
-let over80;
-let starWarsFan;
-
-
-if (usersAge<1999){
-  alert ("Hey! You are over 18! Proceed young Padawan.");
-  over80 = confirm("Are you older than 80? \nHit OK for YES or CANCEL for NO");
-  console.log(over80);
-  if (over80===true){
-    confirm("Do you like Matlock?");
-    alert("I prefer Murder She Wrote \nBye!");
-  } else{
-    starWarsFan = confirm("Are you a Star Wars fan?");
-    if (starWarsFan===true){
-      alert("Impressive. Most impressive. Obi-Wan has taught you well.");
-      alert("You'll make an excellent Jedi!")
-    } else{
-      alert("Consumed you, the darkside has.");
-      alert("Train you I cannot");
-    }
+  if (!(emailArr.includes('@'))) {
+    var yourEmail = prompt('That was not a valid email. Please try again.');
   }
-
-} else {
-  alert ("Sorry young Padawan, your training ends here.");
-  alert("Remember: Your focus determines your reality.")
-
+  else if (!(emailArr.includes('.'))) {
+    var yourEmail = prompt('That was not a valid email. Please try again.');
+  }
+  else {
+    alert('Thanks for providing a valid email.');
+  }
 }
