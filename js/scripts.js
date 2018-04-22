3ad8c5cedd6248bffc15553c9ff357e54259ebcc
felipe hernandez <Felipe_Sue@felipesMini1764.hsd1.ca.comcast.net>
2018-04-22 11:20:08 -0700

    added my notes

--- js/scripts.js
+++ js/scripts.js
@@ -119,3 +119,60 @@ console.log(backwards);
 //
 // let onlyStudents = arr.splice(teacherIndex, 1, 0);
 // console.log(onlyStudents);
+
+
+
+// var myFavs = ['batman', 'black panther', 13 , true, 2003];
+// console.log(myFavs);
+//
+// alert(myFavs.indexOf(true));
+// var favoriteMovie = ['Star Wars VI', '1983'];
+// var secondFavorite = ['Return of the King', '2003'];
+// var thirdFavorite = ['Equilibrium', '2002'];
+// var fourthFavorite =[prompt()];
+// var myFavoriteMovies = [];
+// myFavoriteMovies.push(favoriteMovie, secondFavorite, thirdFavorite, fourthFavorite);
+//
+// console.log(myFavoriteMovies);
+//
+// var movieYears =[];
+// movieYears.push(myFavoriteMovies[0][1], myFavoriteMovies[1][1], myFavoriteMovies[2][1], myFavoriteMovies[3][1],);
+//
+// alert(movieYears);
+
+// let movie1 = ['The Dark Knight', 'Batman'];
+// let movie2 = ['Superman Returns', 'Superman'];
+// let movie3 = ['Guardians of the Galaxy', 'Quill'];
+// let movie4 = ['Avengers', 'Hulk'];
+// let movie5 = ['Wonder Woman', 'Wonder Wo!'];
+//
+// let favoriteMovies = [];
+// favoriteMovies.push(movie1,movie2, movie3, movie4, movie5);
+// console.log('my favorite movies ' + favoriteMovies);
+//
+// let movieTitles = [];
+// movieTitles.push(favoriteMovies[0][0], favoriteMovies[1][0], favoriteMovies[2][0], favoriteMovies[3][0], favoriteMovies[4][0]);
+//
+// alert(movieTitles);
+//
+// // let topFavs = [movie1.pop(), movie2.pop()];
+// //
+// // alert('my favorite ' + movie1.pop() + movie2.pop());
+// // console.log(topFavs)
+//
+// let topFavs = [];
+// topFavs.push(favoriteMovies[0][0], favoriteMovies[1][0]);
+//
+// alert('my favorite ' + topFavs);
+//
+// alert(favoriteMovies.length);
+//
+// let enterThingsToDo = prompt("Hi! List three items to do");
+//
+// let thingsToDo = enterThingsToDo.split('');
+//
+// console.log(thingsToDo);
+// var myString = prompt("hi! enter things to do");
+// var splits = myString.split(' ');
+//
+// console.log(splits);
