bdd0d4cf93de9a80027eb5312eb3cda2d3261d46
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2018-04-23 19:11:48 -0700

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,49 @@
-let shoppingList = [];
+// let shoppingList = [];
+//
+// let item1 = "Detergent";
+// let item2 = "cheese";
+// let item3 = "crackas";
+// let item4 = "ninjas";
+//
+// shoppingList.push(item1, item2, item3);
+//
+// console.log(shoppingList);
 
-let item1 = "Detergent";
-let item2 = "cheese";
-let item3 = "crackas";
-let item4 = "ninjas";
+let movie1 = ['The Dark Knight', 'Batman'];
+let movie2 = ['Superman Returns', 'Superman'];
+let movie3 = ['Guardians of the Galaxy', 'Quill'];
+let movie4 = ['Avengers', 'Hulk'];
+let movie5 = ['Wonder Woman', 'Wonder Wo!'];
 
-shoppingList.push(item1, item2, item3);
+let favoriteMovies = [];
+favoriteMovies.push(movie1,movie2, movie3, movie4, movie5);
+console.log('my favorite movies ' + favoriteMovies);
 
-console.log(shoppingList);
+
+// let i = 0;
+// while (i < favoriteMovies.length) {
+//   console.log(i);
+//   console.log(favoriteMovies[ i ][0])
+//   i++;
+// }
+
+let i;
+for (let i =0; i < favoriteMovies.length; i++) {
+  console.log(i);
+  console.log(favoriteMovies[ i ][0])
+}
+
+
+
+// const movieTitles = [];
+// const topFavs = [];
+// movieTitles.push(favoriteMovies[0][0], favoriteMovies[1][0], favoriteMovies[2][0], favoriteMovies[3][0], favoriteMovies[4][0]);
+//
+// console.log(movieTitles);
+//
+//
+// topFavs.push(favoriteMovies[0][0], favoriteMovies[1][0]);
+//
+// console.log('my favorite ' + topFavs);
+//
+// console.log(favoriteMovies.length);
