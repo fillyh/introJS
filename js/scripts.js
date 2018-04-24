88f04949ac4dc32294be081ca19caa93ec90685b
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2018-04-23 18:27:42 -0700

    day 4 part 2 notes

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+let shoppingList = [];
+
+let item1 = "Detergent";
+let item2 = "cheese";
+let item3 = "crackas";
+let item4 = "ninjas";
+
+shoppingList.push(item1, item2, item3);
+
+console.log(shoppingList);
