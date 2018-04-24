eaecbe24c49be32bf10728d19c3b3cfd05b3c4fc
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2018-04-23 21:00:42 -0700

    notes

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// function sayHello(name, greeting){
+//   return `Hello, ${name}, ${greeting}!`;
+// }
+//
+// let myGreeting = sayHello("Felipe", "You Fly!");
+// alert(myGreeting);
+
+// var fav ="Cheese";
+// function sayMyFav(){
+//   alert(fav);
+// }
+//
+// sayMyFav();
+
+
+// function sayMyFav(){
+//   var fav ="Cheese";   /* will not work - needs to be outside of function to access*/
+// }
+// sayMyFav();
+// alert(fav);
+
+function sayMyFav(){
+  var fav ="Cheese";
+}
