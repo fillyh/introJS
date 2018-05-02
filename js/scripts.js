167ce6578b2f647d6b68bf826330f47fb72802f5
felipe hernandez <Felipe_Sue@felipesMini1764.hsd1.ca.comcast.net>
2018-05-02 00:08:55 -0700

    Day6-HW

--- js/scripts.js
+++ js/scripts.js
@@ -1,88 +1,25 @@
-// // let blueDivs = document.getElementsByClassName('blue');
-// //
-// // let h1= document.getElementsByTagName('h1');
-// //
-// // let para = document.getElementById('para');
-// //
-// // console.log(para);
-// // // console.log(blueDivs[0]);
-// // // console.log(h1[0]);
-// //
-// // let queryBlues = document.querySelector('#para');
-// // console.log(queryBlues);
-// //
-// // let queryBlues2 = document.querySelectorAll('div');
-// // console.log(queryBlues2);
-//
-// //
-// // function queryDom() {
-// //   let body = document.getElementsByTagName('body');
-// //   console.log(body[0]);
-// //   let myBody = document.getElementsByClassName('my-body');
-// //   console.log(myBody);
-// // }
-// // queryDom();
-//
-// // function queryDom(element, method){
-// //   let elementToQuery = document.method(element);
-// //   console.log(body);
-// // }
-// // let el = prompt("Give me an element");
-// // le
-// // queryDom();
-// /*wtf*/
-// const button = document.querySelector('.btn-danger');
-//
-// function logTarget(e){
-//   console.log(e.target);
-// }
-//
-// button.addEventListener('click', (e) =>{
-//   console.log(e);
-//   console.log(e.target);//e.target refers to the actual button ... button info
-//
-// } //callback function because JS is being "called back"
-// )
 
-// const button = document.querySelector('.btn-danger');
-//
-// function logTarget(e){
-//   console.log(e.target);
-// }
-//
-// button.addEventListener('mouseup', (e) =>{
-//   console.log(e);
-//   // console.log(e.target);//e.target refers to the actual button ... button info
-//
-// } //callback function because JS is being "called back"
-// )
-// Create event listeners for each:
-// For the button, alert the user with a message when they click it.
-const button = document.querySelector('.my-btn');
-const div = document.querySelector('.box');
-const h1= document.querySelector('h1');
+// Ask your user to give you a scale (Celcius or Fahrenheit) and the tempurature they want to convert. So if they give you 90 Fahrenheit, they would want you to convert 90 degrees Fahrenheit to whatever that is in Celcius
 
-// button.addEventListener('click', (e) =>{
-//   alert(`Hey! You Clicked on me! ${e.target}`);
+let scale = prompt('Hi, type in a temperature scale', 'Celcius or Fahrenheit').toLowerCase();
+let temperature = parseInt(prompt(`Cool. Now type in a tmeperature` , 123));
+
+// Employ a function that accepts those two values as arguments: temperature and scale (either celcius or fahrenheit) and converts the tempurature they gave you to the opposite scale.
+//Whatever the user enters, I'll need to to make sure that it equals either scale. So lowercase and check to make sure they're equal. use a switch statement?
+
+function convertTemps(scale, temperature){
+  // Display the temperature in an h3 with minimal styling.
+  let h3 = document.querySelector('h3');
+  document.querySelector('h3').textContent = `${temperature} ${scale}`;
+
+//   button.addEventListener('click', (e) => {
+//
+//
 // });
-button.addEventListener('click', (e) =>{
-  let newPTag = document.createElement('p');
-  newPTag.textContent= "Hey there!";
-  div.appendChild(newPTag);
-  div.insertBefore(newPTag, h1);
-});
 
-h1.addEventListener('mouseup', (e) =>{
-  h1.style.color='yellow';
-});
-div.addEventListener('mouseenter', (e) => {div.style.backgroundColor='red'
-});
+};
 
+convertTemps(scale, temperature);
 
-
-
-
-
-
-// For the div, change the color of the background when their mouse goes into the div.
-// For the h1, change the color of the text the user releases any key.
+// From that point on, when the user clicks on the h3, run the function to convert the temperature back and forth between Celcius and Fahrenheit.
+// Display the converted temperature in the h3 each time it is changed.
