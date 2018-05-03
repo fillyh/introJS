664804704b17f5d3d939ae7a4578c0150254d72b
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2018-05-02 20:13:48 -0700

    fizz buzz test

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+// function checkForRed(element){
+//   if(element.classList.contains('red-text')){
+//     element.classList.remove('red-text');
+//   } else {
+//   //add red text
+//   }
+//
+// }
+
+// const myImg = document.querySelector('img');
+//
+//
+// function changeImg(){
+// myImg.setAttribute('src' , 'https://pbs.twimg.com/profile_images/927645314630193158/ufoYTbbi_400x400.jpg')
+// }
+//
+// myImg.addEventListener('click', (e) => {
+// changeImg()
+// })
+
+/*Fizz Buzz Test*/
+// let fizz = 'Fizz';
+// let buzz = 'buzz';
+// let fizzbuzz = 'FizzBuzz';
+//
+// var i;
+// for (i=0; i < 101; i++){
+//
+// }
+// document.getElementById("h3").innerHTML = i;
+// console.log();
+
+const body = document.querySelector('body');
+
+for (let i =1; i<=100; i++) {
+  let p = document.createElement('p');
+  if(i % 3 === 0 && i % 5 === 0){
+    p.textContent = 'FizzBuzz';
+  }else if(i % 3 === 0){
+    p.textContent = 'Fizz';
+  }else if (i % 5 === 0){
+    p.textContent = 'Buzz';
+  }else {
+    p.textContent = i;
+  }
+  body.appendChild(p);
+}
